17b43dc7dc1f648a686d03efc6c342d5b291974b
raulacejo <raulacejo@users.noreply.github.com>
2018-05-18 08:38:07 +0200

    Scraping sencillo con R

---
+++ scraping-in-R.R
@@ -0,0 +1,32 @@
+# source: https://datascienceplus.com/building-a-hacker-news-scraper-with-8-lines-of-r-code-using-rvest-library/
+
+library(rvest)
+
+url <- 'https://news.ycombinator.com/'
+
+#further pages 
+#url2 <- 'https://news.ycombinator.com/news?p=2'
+
+content <- read_html(url)
+
+#News Title
+
+title <- content %>% html_nodes('a.storylink') %>% html_text()
+
+#News Link Domain
+
+link_domain <- content %>% html_nodes('span.sitestr') %>% html_text()
+
+#Link Score / Upvote
+
+score <- content %>% html_nodes('span.score') %>% html_text()
+
+#Link Age (submission time)
+
+age <- content %>% html_nodes('span.age') %>% html_text()
+
+#Final Dataframe
+
+df <- data.frame(title = title, link_domain = link_domain, score = score, age = age)
+#Naive way of extracting the entire page content with this table
+#tb <- content %>% html_node('table.itemlist') %>% html_text()
